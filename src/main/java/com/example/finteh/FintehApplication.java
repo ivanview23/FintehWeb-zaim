df3f8437de3029d9e545ebfcd3b8728c5fdc5e19
cathleeb <cathleeb@student.21-school.ru>
2025-02-24 23:57:28 +0700

    add spring

---
+++ src/main/java/com/example/finteh/FintehApplication.java
@@ -0,0 +1,13 @@
+package com.example.finteh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finteh/FintehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finteh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
